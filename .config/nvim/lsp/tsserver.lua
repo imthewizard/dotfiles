4578ba3dda4ed0b1245a58ee1dc710462bf518ca
The Wizard <62962668+imthewizard@users.noreply.github.com>
2025-06-06 16:44:47 -0300

    fixed tsserver

--- .config/nvim/lsp/tsserver.lua
+++ .config/nvim/lsp/tsserver.lua
@@ -1,3 +1,12 @@
+-- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/ts_ls.lua
 return {
 	cmd = {"typescript-language-server", "--stdio"},
+	filetypes = {
+		'javascript',
+		'javascriptreact',
+		'javascript.jsx',
+		'typescript',
+		'typescriptreact',
+		'typescript.tsx',
+	},
 }
