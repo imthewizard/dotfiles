d5f96bc354e1fcaa0070e6fcac64dc48023bd206
The Wizard <62962668+imthewizard@users.noreply.github.com>
2025-06-27 14:47:50 -0300

    fixed the cpu usage script

--- .config/i3blocks/scripts/cpu.sh
+++ .config/i3blocks/scripts/cpu.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
 
-# top -bn1 | grep Cpu | sed 's/..,//g' | awk '{print "CPU:", 100 - $5 "%"}'
-top -bn1 | grep Cpu | sed 's/..,//g' | awk '{print "CPU:", $2 + $3 + $4 "%"}'
+# Slightly modified script from:
+# https://www.mail-archive.com/linuxkernelnewbies@googlegroups.com/msg01690.html
+
+PREV_TOTAL=0
+PREV_IDLE=0
+
+while true; do
+	CPU=(`cat /proc/stat | grep '^cpu '`) # Get the total CPU statistics.
+	unset CPU[0]                          # Discard the "cpu" prefix.
+	IDLE=${CPU[4]}                        # Get the idle CPU time.
+
+	# Calculate the total CPU time.
+	TOTAL=0
+
+	for VALUE in "${CPU[@]}"; do
+		let "TOTAL=$TOTAL+$VALUE"
+	done
+
+	# Calculate the CPU usage since we last checked.
+	let "DIFF_IDLE=$IDLE-$PREV_IDLE"
+	let "DIFF_TOTAL=$TOTAL-$PREV_TOTAL"
+	let "DIFF_USAGE=(1000*($DIFF_TOTAL-$DIFF_IDLE)/$DIFF_TOTAL+5)/10"
+	echo "CPU: $DIFF_USAGE%"
+
+	# Remember the total and idle CPU times for the next check.
+	PREV_TOTAL="$TOTAL"
+	PREV_IDLE="$IDLE"
+
+	# Wait before checking again.
+	sleep 1
+done
