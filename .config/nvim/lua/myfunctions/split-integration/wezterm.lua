eff9443b141398b1cf2e365ffd1134e1ea2ed9af
The Wizard <62962668+imthewizard@users.noreply.github.com>
2025-07-01 20:33:12 -0300

    fixed wezterm split integration

--- .config/nvim/lua/myfunctions/split-integration/wezterm.lua
+++ .config/nvim/lua/myfunctions/split-integration/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = {}
 ---@param direction Direction
 ---@see dir_to_string
 ---@return string # Same as dir_to_string, but dir.down becomes "bottom" and dir.up "top"
-local function dir_to_string_wez(direction)
+local function dir_to_string_split(direction)
 	if direction.left then
 		return "left"
 	elseif direction.down then
@@ -16,7 +16,7 @@ local function dir_to_string_wez(direction)
 end
 
 ---@param direction Direction
----@see dir_to_string_wez
+---@see dir_to_string_split
 ---@return string # The direction as a string. Ex: direction.left returns "left"
 local function dir_to_string(direction)
 	if direction.left then
@@ -33,7 +33,7 @@ end
 ---Uses wezterm cli to split in the desired direction
 ---@param direction Direction
 function wezterm.split(direction)
-	local dir = dir_to_string_wez(direction)
+	local dir = dir_to_string_split(direction)
 
 	vim.system({"wezterm", "cli", "split-pane", "--"..dir})
 end
@@ -43,15 +43,15 @@ end
 function wezterm.activate_pane(direction)
 	local dir = dir_to_string(direction)
 
-	vim.system({"wezterm", "cli", "activate-pane-direction", "--"..dir})
+	vim.system({"wezterm", "cli", "activate-pane-direction", dir})
 end
 
 ---@param direction Direction
 ---@return boolean # If there exists a pane in the specified direction
 function wezterm.has_pane_direction(direction)
-	local dir = dir_to_string_wez(direction)
+	local dir = dir_to_string(direction)
 
-	local obj = vim.system({"wezterm", "cli", "get-pane-direction", "--"..dir}, {text = true}):wait()
+	local obj = vim.system({"wezterm", "cli", "get-pane-direction", dir}, {text = true}):wait()
 
 	-- Didn't return an id, no pane in that direction
 	if obj.stdout == "" then
