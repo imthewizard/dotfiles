805b035fdb82b310c62cd14b7be2b2b9c4a75715
The Wizard <62962668+imthewizard@users.noreply.github.com>
2025-07-18 16:46:05 -0300

    cleanup

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,19 +1,16 @@
-return { 
+return {
 	"folke/snacks.nvim",
-	--dependencies = { "folke/flash.nvim" },
 	priority = 1000,
 	lazy = false,
 	opts = {
 		bigfile      = {enabled = true},
 		indent       = {enabled = true},
 		picker       = {enabled = true},
-		image        = {enabled = true},
 		quickfile    = {enabled = true},
-		scope        = {enabled = true},
+		image        = {enabled = true},
 		statuscolumn = {enabled = true},
-		words        = {enabled = true},
 	},
-	
+
 	keys = {
 		{ "<leader>pf", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
 		{ "<leader>pb", function() Snacks.picker.buffers() end, desc = "Buffers" },
