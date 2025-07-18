0ebe1669694d60cf9263cdf3c43b5b2edf6a7784
The Wizard <62962668+imthewizard@users.noreply.github.com>
2025-07-18 16:28:33 -0300

    disable flash f t

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -1,7 +1,14 @@
-return { 
+return {
 	"folke/flash.nvim",
   	keys = {
 		{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
 		{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+	},
+	opts = {
+		modes = {
+			char = {
+				enabled = false,
+			},
+		},
 	},
 }
