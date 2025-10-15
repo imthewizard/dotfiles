23fd8d2feaa3f376456806036ecbdf29296ac666
The Wizard <62962668+imthewizard@users.noreply.github.com>
2025-10-15 19:21:41 -0300

    add keybind to snacks image hover

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -7,12 +7,19 @@ return {
 		indent       = {enabled = true},
 		picker       = {enabled = true},
 		quickfile    = {enabled = true},
-		image        = {enabled = true},
+		image = {
+			enabled = true,
+			doc = {
+				enabled = false,
+			},
+		},
 	},
 
 	keys = {
 		{ "<leader>pf", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
 		{ "<leader>pb", function() Snacks.picker.buffers() end, desc = "Buffers" },
 		{ "<leader>pg", function() Snacks.picker.grep() end, desc = "Grep" },
+
+		{ "<leader>sh", function() Snacks.image.hover() end, desc = "Snacks Image Hover" },
 	},
 }
