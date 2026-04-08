5045a20a05e8ab438b23fe3e8586aba8ac6126cd
The Wizard <62962668+imthewizard@users.noreply.github.com>
2026-04-08 05:54:10 -0300

    blink signature

--- .config/nvim/plugin/blink.lua
+++ .config/nvim/plugin/blink.lua
@@ -6,6 +6,11 @@ require("blink.cmp").setup({
 	keymap = {preset = "default"},
 	snippets = {preset = "luasnip"},
 
+	signature = {
+		enabled = true,
+		window = {border = "rounded"},
+	},
+
 	appearance = {
 		-- use_nvim_cmp_as_default = true,
 		nerd_font_variant = "mono",
